c082a4c867230bf7a0bf3032f2cc0658abf95ab7
saga0003 <sagar@smis.edu.in>
2026-09-03 17:47:03 +0530

    Add theme bootstrap

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Print Bureau Premium theme bootstrap.
+ */
+if (!defined('ABSPATH')) { exit; }
+
+require_once get_template_directory() . '/inc/setup.php';
+require_once get_template_directory() . '/inc/content.php';
+require_once get_template_directory() . '/inc/customizer.php';
+require_once get_template_directory() . '/inc/leads.php';
+require_once get_template_directory() . '/inc/seo.php';
