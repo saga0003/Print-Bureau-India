46a25352e2916ff4c00a596db1f62caf18e7ac75
saga0003 <sagar@smis.edu.in>
2026-09-03 17:49:20 +0530

    Add hidden attribution fields for lead capture

---
+++ template-parts/lead-hidden-fields.php
@@ -0,0 +1,10 @@
+<input type="hidden" name="source" value="">
+<input type="hidden" name="medium" value="">
+<input type="hidden" name="campaign" value="">
+<input type="hidden" name="content" value="">
+<input type="hidden" name="term" value="">
+<input type="hidden" name="referrer" value="">
+<input type="hidden" name="landing_page" value="">
+<input type="hidden" name="current_page" value="">
+<input type="hidden" name="device" value="">
+<input class="pbi-honeypot" type="text" name="website" value="" tabindex="-1" autocomplete="off">
