02fee73072a588cc0406e3a4463741eebbda5f97
saga0003 <sagar@smis.edu.in>
2026-09-03 17:51:13 +0530

    Add default page template

---
+++ page.php
@@ -0,0 +1,3 @@
+<?php get_header(); the_post(); ?>
+<article class="pbi-article"><h1><?php the_title(); ?><span class="pbi-dot">.</span></h1><div class="pbi-article__content"><?php the_content(); ?></div></article>
+<?php get_footer(); ?>
