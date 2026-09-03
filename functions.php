2ab1ae7e302668cc6b0d6040a389cb07f41497a0
saga0003 <sagar@smis.edu.in>
2026-09-04 04:21:58 +0530

    Add automatic GitHub theme sync bootstrap

--- functions.php
+++ functions.php
@@ -9,3 +9,4 @@ require_once get_template_directory() . '/inc/content.php';
 require_once get_template_directory() . '/inc/customizer.php';
 require_once get_template_directory() . '/inc/leads.php';
 require_once get_template_directory() . '/inc/seo.php';
+require_once get_template_directory() . '/inc/github-sync.php';
