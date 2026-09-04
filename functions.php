2285c3b9a59031c6ecb3b014ba777aec25e3decc
saga0003 <sagar@smis.edu.in>
2026-09-04 11:49:02 +0530

    Load GitHub-managed theme asset helpers

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@
 if (!defined('ABSPATH')) { exit; }
 
 require_once get_template_directory() . '/inc/setup.php';
+require_once get_template_directory() . '/inc/theme-assets.php';
 require_once get_template_directory() . '/inc/content.php';
 require_once get_template_directory() . '/inc/customizer.php';
 require_once get_template_directory() . '/inc/leads.php';
