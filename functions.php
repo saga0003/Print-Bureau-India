3a0b74f58d24d54193b4da4d63ec1db11dbf5acf
saga0003 <sagar@smis.edu.in>
2026-09-12 05:56:19 +0530

    Load product edit button styling helper

--- functions.php
+++ functions.php
@@ -23,6 +23,7 @@ require_once get_template_directory() . '/inc/product-gallery.php';
 require_once get_template_directory() . '/inc/content-sync.php';
 require_once get_template_directory() . '/inc/frontend-content-manager.php';
 require_once get_template_directory() . '/inc/content-manager-user-setup.php';
+require_once get_template_directory() . '/inc/frontend-edit-button.php';
 require_once get_template_directory() . '/inc/customizer.php';
 require_once get_template_directory() . '/inc/leads.php';
 require_once get_template_directory() . '/inc/seo.php';
